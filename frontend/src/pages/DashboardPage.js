10777776b65c7c5a3e2a3dd78f70c3cc8f628a76
Piero <piero.neri@hotmail.fr>
2024-01-09 12:47:28 +0100

    Add front project structure (dashboard and card components) + router

---
+++ frontend/src/pages/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>Tableau de bord</h1>
+            <Dashboard />
+        </div>
+    );
+};
+
+export default DashboardPage;
