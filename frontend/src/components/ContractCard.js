19fec2b5691a180d7ded636ecdf21458388161e3
pneri <piero.neri@pole-emploi.fr>
2024-01-09 15:30:51 +0100

    stylize the dashboard and cards

--- frontend/src/components/ContractCard.js
+++ frontend/src/components/ContractCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import '../styles/_contractCard.scss';
 const ContractCard = ({ contract }) => {
     return (
         <div className="contract-card">
-            <h3>{contract.title}</h3>
-            <p>{contract.description}</p>
+            <img className="contract-card__image" src = {"img/contract-" + (Math.floor(Math.random() * (3 - 1)) + 1) + ".jpeg"} alt="contract" />
+            <div className="contract-card__info">
+                <div className="contract-card__info-text">
+                    <h3>{contract.title}</h3>
+                    <p>{contract.description}</p>
+                </div>
+                <button className="contract-card__info-action">Signer</button>
+
+            </div>
         </div>
     );
 };
