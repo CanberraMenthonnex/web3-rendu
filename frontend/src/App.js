e08ebb86913597fffaa2878cf24bcdbf9ce218ca
Piero <piero.neri@hotmail.fr>
2024-01-09 13:52:21 +0100

    first styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
+import Sidebar from "./components/Sidebar";
+import './styles/_app.scss';
 
 const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<DashboardPage />} />
-            </Routes>
+            <div className="app-container">
+                <Sidebar />
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/" element={<DashboardPage />} />
+                        {/* Ajoutez d'autres routes pour chaque page */}
+                    </Routes>
+                </div>
+            </div>
         </Router>
     );
 };
+
+
 
 export default App;
