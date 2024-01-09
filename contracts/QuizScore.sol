2a5aea0da92826c393445fbf7e70447724af7d91
Sverker <maxime.attala@gmail.com>
2024-01-09 15:37:04 +0100

    modified: QuizScore.sol for NFTStorage usage by adding tokenURI

--- contracts/QuizScore.sol
+++ contracts/QuizScore.sol
@@ -1,17 +1,20 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract QuizScore is ERC1155, Ownable {
-    uint256 private _tokenIdCounter = 0;
-    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+contract QuizScore is ERC1155URIStorage, Ownable {
+    uint256 private _tokenId;
 
-    function mintNFT(address _to, uint256 _tokenId)
-        external
-        onlyOwner {
-        _tokenIdCounter++;
-        _mint(_to, _tokenId, 1, "");
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner){}
+
+    function awardItem(address to, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256){
+            _tokenId++;
+            _mint(to, _tokenId, 1, "");
+            _setURI(_tokenId, tokenURI);
+            return _tokenId;
     }
 }
