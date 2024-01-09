3142f8065702f12923a8b220e208093a96c2b4fb
Sverker <maxime.attala@gmail.com>
2024-01-09 15:39:48 +0100

    modified: re add license

--- contracts/QuizScore.sol
+++ contracts/QuizScore.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
@@ -17,4 +18,4 @@ contract QuizScore is ERC1155URIStorage, Ownable {
             _setURI(_tokenId, tokenURI);
             return _tokenId;
     }
-}
+}a
