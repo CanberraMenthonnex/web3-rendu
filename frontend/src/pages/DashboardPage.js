8ad82ffffc11fe035dc48a922a7ff54463499dd8
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-01-10 15:55:42 +0100

    [fix/fix-hook-connection][fix]:fix logout function

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Dashboard from '../components/Dashboard';
 import Header from '../components/Header';
-import { useConnectWallet, useWallets } from '@web3-onboard/react';
 import Sidebar from '../components/Sidebar';
 import '../styles/_dashboard.scss';
 import { useCustomWalltetConnection } from '../hooks/useCustomWalltetConnection';
 
 const DashboardPage = () => {
-  const { wallet, handleConnect, accountAddress, connecting } =
+  const { handleConnect, accountAddress, connecting } =
     useCustomWalltetConnection();
 
   return (
